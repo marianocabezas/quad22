3e9e885bd59d7b35aae2d10aaa63d9c15c5e7103
mariano <mcabezas@eia.udg.edu>
2022-05-23 12:19:50 +1000

    bug fix

--- base.py
+++ base.py
@@ -792,10 +792,12 @@ class PairedAttention(nn.Module):
         value = value.movedim((3, 4, 5), (1, 2, 3))
         value_token = value.flatten(0, -3)
 
-        att = torch.bmm(query_token.transpose(1, 2), key_token)
+        att = torch.bmm(key_token, query_token.transpose(1, 2))
         att_map = self.norm(att / np.sqrt(self.features))
         print(att_map.shape, value_token.shape)
-        features = torch.bmm(att_map, value_token).view(query.shape)
+        features = torch.bmm(
+            value_token.transpose(1, 2), att_map
+        ).transpose(1, 2).view(query.shape)
 
         return features.movedim((1, 2, 3), (3, 4, 5))
 
